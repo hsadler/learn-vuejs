12087b48659bb1ae114d3257285dae8ae455d97f
Harry Sadler <harrysadlermusic@gmail.com>
2018-03-12 19:23:23 -0700

    add component computed example

---
+++ src/components/components-test/ComponentTestComputed.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>{{ $options.name }}</h2>
+    <p>{{ greeting }}</p>
+    <p>{{ compAge }}</p>
+    <button @click="compAge = age + 1">Add to age</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentTestComputed',
+  data () {
+    return {
+      firstName: 'Harry',
+      lastName: 'Sadler',
+      age: 36
+    }
+  },
+  computed: {
+    greeting () {
+      return 'Hi! My name is ' + this.firstName + ' ' + this.lastName + '.'
+    },
+    compAge: {
+      get () {
+        return 'computed age: ' + this.age
+      },
+      set (newValue) {
+        this.age = parseInt(newValue)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  div {
+    width: 400px;
+    margin: 10px auto;
+    border: 1px solid green;
+    h2 {
+      color: green;
+    }
+    button {
+      margin-bottom: 20px;
+    }
+  }
+</style>
--- src/views/ComponentsTest.vue
+++ src/views/ComponentsTest.vue
@@ -2,16 +2,19 @@
   <div class="about">
     <h1>Components Test</h1>
     <ComponentTestData></ComponentTestData>
+    <ComponentTestComputed></ComponentTestComputed>
   </div>
 </template>
 
 <script>
 import ComponentTestData from '@/components/components-test/ComponentTestData'
+import ComponentTestComputed from '@/components/components-test/ComponentTestComputed'
 
 export default {
   name: 'ComponentsTest',
   components: {
-    ComponentTestData
+    ComponentTestData,
+    ComponentTestComputed
   }
 }
 </script>
