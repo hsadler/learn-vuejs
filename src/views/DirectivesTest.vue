9c16404dfd1fe878afdf359172569ea46514b1bc
Harry Sadler <harrysadlermusic@gmail.com>
2018-03-13 22:40:48 -0700

    add more directive examples

--- src/views/DirectivesTest.vue
+++ src/views/DirectivesTest.vue
@@ -18,19 +18,48 @@
 
     <hr>
 
-    <p v-if="mode === 0">v-if: mode={{ mode }}</p>
-    <p v-else-if="mode === 1">v-else-if: mode={{ mode }}</p>
-    <p v-else>v-else: mode={{ mode }}</p>
+    <p v-if="mode === 0">v-if: <span>mode={{ mode }}</span></p>
+    <p v-else-if="mode === 1">v-else-if: <span>mode={{ mode }}</span></p>
+    <p v-else>v-else: <span>mode={{ mode }}</span></p>
     <button @click="updateMode">update mode</button>
 
     <hr>
 
+    <p>v-for:</p>
     <p
+      class="green"
       v-for="(score, index) in scores"
       :key="score"
     >
         {{ index + ': ' + score }}
     </p>
+
+    <hr>
+
+    <p>v-on:</p>
+    <p class="green">keydown key: {{ currEvent ? currEvent.key : 'none' }}</p>
+    <input
+      v-on:keydown="viewEvent($event)"
+      placeholder="type here"
+    >
+
+    <hr>
+
+    <p>v-bind:</p>
+    <p v-bind:class="classStr">Style me with class: {{ classStr }}</p>
+
+    <hr>
+
+    <p>v-model:</p>
+    <textarea v-model="textareaInput" placeholder="type here"></textarea>
+    <p>
+      <strong>What you've entered:</strong><br>
+      <span>{{textareaInput}}</span>
+    </p>
+
+    <hr>
+
+    <p v-pre>v-pre: <span>{{ notCompiled }}</span></p>
 
   </div>
 </template>
@@ -44,7 +73,11 @@ export default {
       myHtml: '<span style="font-weight:bold">I am bold.</span>',
       isShown: false,
       mode: 0,
-      scores: [123, 456, 789]
+      scores: [123, 456, 789],
+      currEvent: null,
+      classStr: 'purple',
+      textareaInput: null,
+      notCompiled: 'I will never be injected into to template'
     }
   },
   methods: {
@@ -56,6 +89,9 @@ export default {
       if (this.mode > 2) {
         this.mode = 0
       }
+    },
+    viewEvent (e) {
+      this.currEvent = e
     }
   }
 }
@@ -66,12 +102,22 @@ export default {
     width: 400px;
     margin: 0 auto;
     padding-bottom: 100px;
+    .green {
+      color: green;
+    }
+    .purple {
+      color: purple;
+    }
     p {
       display: block;
       margin: 10px 0;
       span {
         color: green;
       }
+    }
+    textarea {
+      width: 300px;
+      height: 60px;
     }
   }
 </style>
